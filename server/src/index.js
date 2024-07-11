210fe3ad4cc10c87cd049b890c2027d9ffb5003d
Jason Bullard <jason@bullard.io>
2024-07-11 14:56:26 -0500

    add mock fleet data for server
    Co-authored-by: moomoomancow <moomoomancow@users.noreply.github.com>
    Co-authored-by: rmorrow87 <rmorrow87@users.noreply.github.com>
    Co-authored-by: wballard7 <wballard7@users.noreply.github.com>
    Co-authored-by: ausray1294 <ausray1294@users.noreply.github.com>

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const fleet = require("./data/fleet");
 
 const PORT = 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (_, res) => {
-  res.status(200).json({ msg: "Hello" });
+  res.status(200).json(fleet);
+});
+
+app.get("/:vin", (req, res) => {
+  const vin = req.params.vin;
+  const vehicle = fleet.find((v) => v.vehicle_vin === vin);
+  res.status(200).json(vehicle);
 });
 
 app.listen(PORT, () => {
