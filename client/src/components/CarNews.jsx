9f8b973627031fb246e72e8d522063d1b3472c7d
Jason Bullard <jason@bullard.io>
2024-07-15 16:59:26 -0500

    filter out unwanted news articles

--- client/src/components/CarNews.jsx
+++ client/src/components/CarNews.jsx
@@ -1,4 +1,44 @@
 import React, { useEffect, useState } from "react";
+import { List, UnorderedList, ListItem, Heading, Box } from "@chakra-ui/react";
+
+const cleanCarNews = (articles) => {
+  const unwantedKeyWords = [
+    "drug",
+    "death",
+    "kill",
+    "dead",
+    "murder",
+    "violence",
+    "gun",
+    "violence",
+    "jail",
+    "prison",
+    "shots",
+    "weapon",
+    "beer",
+    "alcohol",
+    "dies",
+    "stolen",
+    "bad words",
+    // "[Removed]",
+  ];
+
+  const containsUnwantedKeywords = (text) => {
+    if (!text) return false;
+    return unwantedKeyWords.some((keyword) =>
+      text.toLowerCase().includes(keyword),
+    );
+  };
+
+  //filter the car data to ignore [Removed];
+  return articles.filter(
+    (article) =>
+      article.title !== "[Removed]" &&
+      article.description !== "[Removed]" &&
+      !containsUnwantedKeywords(article.title) &&
+      !containsUnwantedKeywords(article.description),
+  );
+};
 
 const CarNews = () => {
   const [news, setNews] = useState([]);
@@ -14,9 +54,11 @@ const CarNews = () => {
           throw new Error("Network response failed");
         }
         const data = await res.json();
-        setNews(data.articles);
-      } catch {
-        // console.error('error', error.message)
+        const cleanedData = cleanCarNews(data.articles);
+        setNews(cleanedData);
+        console.log(cleanedData);
+      } catch (error) {
+        console.error("error", error);
       } finally {
         setLoading(false);
       }
@@ -30,17 +72,20 @@ const CarNews = () => {
   }
 
   return (
-    <div>
-      <ul className="newsList">
-        {news.map((article, index) => (
-          <li key={index}>
-            <a href={article.url} target="blank" rel="article">
-              {article.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Box>
+      <Heading>Car Related News</Heading>
+      <List>
+        <UnorderedList className="newsList">
+          {news.map((article, index) => (
+            <ListItem className="newsArticle" key={index}>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                {article.title}
+              </a>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </List>
+    </Box>
   );
 };
 
