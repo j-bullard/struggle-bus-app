933680cbaa1cb0e792d73ccee55a8a9ecc947f6c
Jason Bullard <jason@bullard.io>
2024-07-15 17:00:13 -0500

    change to path for refactored component

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,10 +1,17 @@
-import { Box, Button, Container, Stack } from "@chakra-ui/react";
+import { Box, Button, Container, Stack, Image } from "@chakra-ui/react";
 import { NavLink, Outlet } from "react-router-dom";
 import { RxDashboard, RxMagnifyingGlass, RxRocket } from "react-icons/rx";
 
 const Layout = () => {
   return (
     <Box>
+      {/* <Image
+        borderRadius="full"
+        boxSize="100px"
+        // src="../image/GovV-logo.png"
+        src="https://files.oaiusercontent.com/file-NQk1gOELxtHzKcl8fbZqOGZ2?se=2024-07-15T19%3A39%3A23Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D848b0145-f1ea-428f-add4-ee95c82e46f3.webp&sig=5F58d4phycQXjX9U8XdHtRJRDlOXYxfNI%2BFhKpTXUVU%3D"
+        alt="Gov V"
+      /> */}
       <Stack
         sx={{
           width: "16rem",
@@ -32,7 +39,7 @@ const Layout = () => {
         </Button>
         <Button
           as={NavLink}
-          to="/browse"
+          to="/avl"
           variant="ghost"
           leftIcon={<RxMagnifyingGlass fontSize={20} />}
           sx={{
