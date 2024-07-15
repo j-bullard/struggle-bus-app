c16f1cc306e9382a4751119a9c3c5425247219c9
Jason Bullard <jason@bullard.io>
2024-07-15 17:00:26 -0500

    refactor with chakra-ui

--- client/src/components/SingleVehicleDetails.jsx
+++ client/src/components/SingleVehicleDetails.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import {
+  Box,
+  Heading,
+  OrderedList,
+  UnorderedList,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 
 const SingleVehicleDetails = () => {
   const [carDetails, setCarDetails] = useState();
-  const { id } = useParams();
+  const { vin } = useParams();
 
   useEffect(() => {
-    fetch(`/api/trims/${id}`)
+    fetch(`/api/fleet/${vin}`)
       .then((res) => res.json())
       .then((data) => {
         setCarDetails(data);
       });
-  }, [id]);
+  }, [vin]);
 
   if (!carDetails) {
     return <p>Loading...</p>;
@@ -19,111 +27,148 @@ const SingleVehicleDetails = () => {
 
   return (
     <>
-      <div className="DetailsPage">
-        <h1>
+      <Box className="DetailsPage">
+        <Heading>
           {" "}
+          {console.log(carDetails)}
           {carDetails.year} {carDetails.make_model.make.name}{" "}
           {carDetails.make_model.name}
-        </h1>
+        </Heading>
         <p>
           <strong>{carDetails.description}</strong>
         </p>
-        <img src="/images/MAter.jpg" alt="Car Image" />
-        <ul className="DetailsList">
-          <li>Body Type: {carDetails.make_model_trim_body.type}</li>
-          <li>Trim: {carDetails.name}</li>
-          <li>Description: {carDetails.description}</li>
-          <li>Doors: {carDetails.make_model_trim_body.doors}</li>
-          <li>Seats: {carDetails.make_model_trim_body.seats}</li>
-          <li>
-            Cargo Capacity: {carDetails.make_model_trim_body.cargo_capacity}
-          </li>
-          <li>
-            Max Cargo '('lbs')':{" "}
-            {carDetails.make_model_trim_body.max_cargo_capacity}
-          </li>
-          <li>
-            Max Payload '('lbs')': {carDetails.make_model_trim_body.max_payload}
-          </li>
-          <li>
-            Max Towing '('lbs')':{" "}
-            {carDetails.make_model_trim_body.max_towing_capacity}
-          </li>
-          <li>MSRP: {carDetails.msrp}</li>
-          <li>
-            {" "}
-            Combined MPG '('Gasoline')':{" "}
-            {carDetails.make_model_trim_mileage.combined_mpg}
-          </li>
-          <li>
-            {" "}
-            Combined MPG '('Electric')':{" "}
-            {carDetails.make_model_trim_mileage.epa_combined_mpg_electric}
-          </li>
-          <li>Engine Type: {carDetails.make_model_trim_engine.engine_type}</li>
-          <li>Fuel Type: {carDetails.make_model_trim_engine.fuel_type}</li>
-          <li>Drive Type: {carDetails.make_model_trim_engine.drive_type}</li>
-          <li>
-            Transmission: {carDetails.make_model_trim_engine.transmission}
-          </li>
-
+        <img src={carDetails.image} alt="Car Image" />
+        <List>
+          <UnorderedList className="DetailsList">
+            <ListItem>
+              Body Type: {carDetails.make_model_trim_body.type}
+            </ListItem>
+            <ListItem>Trim: {carDetails.name}</ListItem>
+            <ListItem>VIN: {carDetails.vin}</ListItem>
+            <ListItem>Doors: {carDetails.make_model_trim_body.doors}</ListItem>
+            <ListItem>Seats: {carDetails.make_model_trim_body.seats}</ListItem>
+            <ListItem>
+              Cargo Capacity: {carDetails.make_model_trim_body.cargo_capacity}
+            </ListItem>
+            <ListItem>
+              Max Cargo (lbs):{" "}
+              {carDetails.make_model_trim_body.max_cargo_capacity}
+            </ListItem>
+            <ListItem>
+              Max Payload (lbs): {carDetails.make_model_trim_body.max_payload}
+            </ListItem>
+            <ListItem>
+              Max Towing (lbs):{" "}
+              {carDetails.make_model_trim_body.max_towing_capacity}
+            </ListItem>
+            <ListItem>MSRP: {carDetails.msrp}</ListItem>
+            <ListItem>
+              {" "}
+              Combined MPG (Gasoline):{" "}
+              {carDetails.make_model_trim_mileage.combined_mpg}
+            </ListItem>
+            <ListItem>
+              {" "}
+              Combined MPG (Electric):{" "}
+              {carDetails.make_model_trim_mileage.epa_combined_mpg_electric}
+            </ListItem>
+            <ListItem>
+              Engine Type: {carDetails.make_model_trim_engine.engine_type}
+            </ListItem>
+            <ListItem>
+              Fuel Type: {carDetails.make_model_trim_engine.fuel_type}
+            </ListItem>
+            <ListItem>
+              Drive Type: {carDetails.make_model_trim_engine.drive_type}
+            </ListItem>
+            <ListItem>
+              Transmission: {carDetails.make_model_trim_engine.transmission}
+            </ListItem>
+          </UnorderedList>
+        </List>
+        <List>
           {carDetails.make_model_trim_body.type === "Sedan" && (
             <>
-              <li>Change Oil: 8000 miles </li>
-              <li>Change Transmission Fluid: 10000 </li>
-              <li>Change Brake Fluid: 7000 </li>
-              <li>Change Brakepads: 60000 </li>
-              <li>Change Tires: 10000</li>
-              <li>Tire Rotation: 5000 </li>
-              <li>Change Spark Plugs: 50000</li>
+              <Heading as="h2" size="2x1">
+                Maintenance Requirements (in miles)
+              </Heading>
+              <OrderedList>
+                <ListItem>Change Oil: 8000 </ListItem>
+                <ListItem>Change Transmission Fluid: 10000 </ListItem>
+                <ListItem>Change Brake Fluid: 7000 </ListItem>
+                <ListItem>Change Brakepads: 60000 </ListItem>
+                <ListItem>Change Tires: 10000</ListItem>
+                <ListItem>Tire Rotation: 5000 </ListItem>
+                <ListItem>Change Spark Plugs: 50000</ListItem>
+              </OrderedList>
             </>
           )}
 
           {carDetails.make_model_trim_body.type === "SUV" && (
             <>
-              <li>Change Oil: 8000 miles </li>
-              <li>Change Transmission Fluid: 10000 </li>
-              <li>Change Brake Fluid: 7000 </li>
-              <li>Change Brakepads: 20000 </li>
-              <li>Change Tires: 10000</li>
-              <li>Tire Rotation: 5000 </li>
-              <li>Change Spark Plugs: 40000</li>
+              <Heading as="h2" size="2x1">
+                Maintenance Requirements (in miles)
+              </Heading>
+              <OrderedList>
+                <ListItem>Change Oil: 8000 miles </ListItem>
+                <ListItem>Change Transmission Fluid: 10000 </ListItem>
+                <ListItem>Change Brake Fluid: 7000 </ListItem>
+                <ListItem>Change Brakepads: 20000 </ListItem>
+                <ListItem>Change Tires: 10000</ListItem>
+                <ListItem>Tire Rotation: 5000 </ListItem>
+                <ListItem>Change Spark Plugs: 40000</ListItem>
+              </OrderedList>
             </>
           )}
 
           {carDetails.make_model_trim_body.type === "Coupe" && (
             <>
-              <li>Change Oil: 8000 miles </li>
-              <li>Change Transmission Fluid: 10000 </li>
-              <li>Change Brake Fluid: 7000 </li>
-              <li>Change Brakepads: 60000 </li>
-              <li>Change Tires: 10000</li>
-              <li>Tire Rotation: 5000 </li>
-              <li>Change Spark Plugs: 40000</li>
+              <Heading as="h2" size="2x1">
+                Maintenance Requirements (in miles)
+              </Heading>
+              <OrderedList>
+                <ListItem>Change Oil: 8000 miles </ListItem>
+                <ListItem>Change Transmission Fluid: 10000 </ListItem>
+                <ListItem>Change Brake Fluid: 7000 </ListItem>
+                <ListItem>Change Brakepads: 60000 </ListItem>
+                <ListItem>Change Tires: 10000</ListItem>
+                <ListItem>Tire Rotation: 5000 </ListItem>
+                <ListItem>Change Spark Plugs: 40000</ListItem>
+              </OrderedList>
             </>
           )}
 
           {carDetails.make_model_trim_body.type === "Convertible" && (
             <>
-              <li>Change Oil: 8000 miles </li>
-              <li>Change Transmission Fluid: 10000 </li>
-              <li>Change Brake Fluid: 7000 </li>
-              <li>Change Brakepads: 60000 </li>
-              <li>Change Tires: 10000</li>
-              <li>Tire Rotation: 5000 </li>
-              <li>Change Spark Plugs: 50000</li>
+              <Heading as="h2" size="2x1">
+                Maintenance Requirements (in miles)
+              </Heading>
+              <OrderedList>
+                <ListItem>Change Oil: 8000 miles </ListItem>
+                <ListItem>Change Transmission Fluid: 10000 </ListItem>
+                <ListItem>Change Brake Fluid: 7000 </ListItem>
+                <ListItem>Change Brakepads: 60000 </ListItem>
+                <ListItem>Change Tires: 10000</ListItem>
+                <ListItem>Tire Rotation: 5000 </ListItem>
+                <ListItem>Change Spark Plugs: 50000</ListItem>
+              </OrderedList>
             </>
           )}
 
           {carDetails.make_model_trim_body.type === "Hatchback" && (
             <>
-              <li>Change Oil: 8000 miles </li>
-              <li>Change Transmission Fluid: 10000 </li>
-              <li>Change Brake Fluid: 7000 </li>
-              <li>Change Brakepads: 60000 </li>
-              <li>Change Tires: 10000</li>
-              <li>Tire Rotation: 5000 </li>
-              <li>Change Spark Plugs: 50000</li>
+              <Heading as="h2" size="2x1">
+                Maintenance Requirements (in miles)
+              </Heading>
+              <OrderedList>
+                <ListItem>Change Oil: 8000 miles </ListItem>
+                <ListItem>Change Transmission Fluid: 10000 </ListItem>
+                <ListItem>Change Brake Fluid: 7000 </ListItem>
+                <ListItem>Change Brakepads: 60000 </ListItem>
+                <ListItem>Change Tires: 10000</ListItem>
+                <ListItem>Tire Rotation: 5000 </ListItem>
+                <ListItem>Change Spark Plugs: 50000</ListItem>
+              </OrderedList>
             </>
           )}
 
@@ -131,17 +176,22 @@ const SingleVehicleDetails = () => {
             .toLowerCase()
             .includes("truck") && (
             <>
-              <li>Change Oil: 8000 miles </li>
-              <li>Change Transmission Fluid: 10000 </li>
-              <li>Change Brake Fluid: 7000 </li>
-              <li>Change Brakepads: 60000 </li>
-              <li>Change Tires: 10000</li>
-              <li>Tire Rotation: 5000 </li>
-              <li>Change Spark Plugs: 50000</li>
+              <Heading as="h2" size="2x1">
+                Maintenance Requirements (in miles)
+              </Heading>
+              <OrderedList>
+                <ListItem>Change Oil: 8000 miles </ListItem>
+                <ListItem>Change Transmission Fluid: 10000 </ListItem>
+                <ListItem>Change Brake Fluid: 7000 </ListItem>
+                <ListItem>Change Brakepads: 60000 </ListItem>
+                <ListItem>Change Tires: 10000</ListItem>
+                <ListItem>Tire Rotation: 5000 </ListItem>
+                <ListItem>Change Spark Plugs: 50000</ListItem>
+              </OrderedList>
             </>
           )}
-        </ul>
-      </div>
+        </List>
+      </Box>
     </>
   );
 };
